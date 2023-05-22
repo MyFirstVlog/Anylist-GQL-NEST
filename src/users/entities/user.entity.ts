18ebbd56e900dd0d821361295f677321a793cd84
Alejandro Estrada <alejoestradam@yahoo.com>
2023-05-21 19:04:03 -0500

    user entity on db

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,39 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({name: 'users'})
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column('text')
+  @Field(() => String)
+  fullName: string;
+
+  @Column('text', {unique: true})
+  @Field(() => String)
+  email: string;
+
+  @Column('text')
+  // @Field(() => String) //* If i DONT WANT TO EXPOSE IT IN GRAPHQL
+  password: string;
+
+  @Column({
+    type: 'text',
+    array: true,
+    default: ['user']
+  })
+  @Field(() => [String])
+  roles: string[];
+
+  @Column({
+    type: 'boolean',
+    default: true
+  })
+  @Field(() => Boolean)
+  isBlocked: boolean;
+
 }
