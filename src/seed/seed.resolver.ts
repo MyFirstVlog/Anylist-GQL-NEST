f2be34525b4e94f93bf8fa2bbe4a03d724acc6be
Alejandro Estrada <alejoestradam@yahoo.com>
2023-06-02 10:49:55 -0500

    prod routes protection

--- src/seed/seed.resolver.ts
+++ src/seed/seed.resolver.ts
@@ -1,12 +1,16 @@
 import { Mutation, Resolver } from '@nestjs/graphql';
 import { SeedService } from './seed.service';
+import { ConfigService } from '@nestjs/config';
 
 @Resolver()
 export class SeedResolver {
-  constructor(private readonly seedService: SeedService) {}
+  
+  constructor(
+    private readonly seedService: SeedService
+  ) {}ß
 
   @Mutation(() => Boolean)
   async executeSeed(): Promise<Boolean>{
-    return true;
+    return this.seedService.executeSeed();
   }
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class SeedService {}
+export class SeedService {
+    isProd: boolean;
+
+  constructor(
+    private readonly configService :ConfigService
+  ) {
+    this.isProd = this.configService.get('STATE') === 'prod';
+  }
+
+  async executeSeed(){
+
+    //? Routes protection
+    if(this.isProd) throw new UnauthorizedException('Seeds are not available on prod');
+    
+    return true;
+  }
+}
